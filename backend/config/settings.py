e4f5f7e13b45c42faf9ab6156797cb9ff4482f90
Nissi Tendo <mayanjanissitendo@gmail.com>
2026-06-08 01:08:58 +0300

    feat: add integration, auth endpoints and session handling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "corsheaders",
     "iles",
 ]
@@ -79,6 +80,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ],
